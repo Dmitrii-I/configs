8cde760f468ddc3793feb22c4f0b78c9a368c0fe
D. I <izgurskii@gmail.com>
2018-12-07 14:25:00 +0100

    Install vimrc and vim plugins

--- install-all-configs.sh
+++ install-all-configs.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
+set -x
 
 ln -sf ~/configs/bashrc ~/.bashrc
 ln -sf ~/configs/bashrc ~/.bash_profile
+ln -sf ~/configs/vimrc ~/.vimrc
+
+
+
+[ -d ~/.vim/bundle/vim-colors-solarized ] && rm -rf ~/.vim/bundle
+mkdir -p ~/.vim/bundle
+mkdir -p ~/.vim/autoload
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+cd ~/.vim/bundle
+git clone git://github.com/altercation/vim-colors-solarized.git
+git clone git://github.com/vim-airline/vim-airline
 
