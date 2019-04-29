893aa4de56e61d9546e0f1647fb67ba1c326f983
D. I <izgurskii@gmail.com>
2019-04-29 17:39:45 +0200

    Set options to exit on failure

--- install-all-configs.sh
+++ install-all-configs.sh
@@ -1,13 +1,35 @@
 #!/usr/bin/env bash
-set -x
+
+set -o nounset      # exit with non-zero status if expansion is attempted on an unset variable
+set -o errexit      # exit immediatelly if a pipeline, a list, or a compound command fails
+set -o pipefail     # failures in pipe in the commands before last one, also count as failures
+
+# Convenience functions from: https://github.com/Dmitrii-I/bash-scripts/blob/master/lib-bash-generic.sh
+source /opt/tutti-dw/symlinks/tutti-env-vars.sh
+
+# Trapping non-zero exit codes:
+on_error() {
+    line_num="$1"
+    echo "Caught error on line $line_num"
+}
+
+on_exit() {
+    true
+}
+
+on_interrupt() {
+    true
+}
+trap 'on_error $LINENO' ERR
+trap on_exit EXIT
+trap on_interrupt INT
+
 
 ln -sf ~/configs/bashrc ~/.bashrc
 ln -sf ~/configs/bashrc ~/.bash_profile
 ln -sf ~/configs/vimrc ~/.vimrc
 ln -sf ~/configs/ansible.cfg ~/.ansible.cfg
 ln -sf ~/configs/sqliterc ~/.sqliterc
-
-
 
 [ -d ~/.vim/bundle/vim-colors-solarized ] && rm -rf ~/.vim/bundle
 mkdir -p ~/.vim/bundle
